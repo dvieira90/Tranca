fc3cd9070a9435d64deff3ae974667aa08b3e6c1
Administrator <danilo_oliveira@ufg.br>
2021-02-12 18:06:10 -0300

    começando o db

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,11 +1,51 @@
 #!/usr/bin/python3
+from .pages.homepage import homepage
+from .api.node import node
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+
+db = SQLAlchemy(app)
 
 
-from .api.node import node
-from .pages.homepage import homepage
+class Users(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    login = db.Column(db.String(100))
+
+    def __init__(self, login):
+        self.login = login
+
+
+class Tags(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100))
+    tag = db.Column(db.String(20))
+
+    def __init__(self, nome, tag):
+        self.nome = nome
+        self.tag = tag
+
+
+class Portas(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100))
+
+
+class Portas_Users():
+    portas_id = db.Column(db.Integer, db.ForeignKey(
+        'portas.id'), nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), nullable=False)
+
+
+class User_Tags():
+    tags_id = db.Column(db.Integer, db.ForeignKey(
+        'tags.id'), nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), nullable=False)
+
 
 app.register_blueprint(homepage)
 app.register_blueprint(node)
--- server/run.py
+++ server/run.py
@@ -1,3 +1,5 @@
 from app import app
 
 app.run(debug=True)
+
+app.db.create_all()
