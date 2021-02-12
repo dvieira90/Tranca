eae80179b33d8221a47219b9584ba44450c55fe8
Administrator <danilo_oliveira@ufg.br>
2021-02-12 14:57:01 -0300

    recomeçando

--- Server/controller/api.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from ..server import app
-
-
-@app.route('/', methods=['GET', ])
-def urls():
-    return "Teste"
--- Server/server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from flask import Flask
-
-app = Flask(__name__)
-app.secret_key = 'alura'
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='localhost')
